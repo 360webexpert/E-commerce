06a15cf651f5b4d9355b58c8ada4369b00d04e14
360webexpert <360webexpert@gmail.com>
2024-06-04 16:57:51 +0530

    create structure

---
+++ server/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+    // MongoDB connection URI
+    mongoURI: 'mongodb://localhost:27017/ecommerce'
+  };
+  
---
+++ server/server.js
@@ -0,0 +1,41 @@
+
+
+
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+require('dotenv').config();
+
+// const employer = require('./routes/employee.routes');
+// const users = require("./routes/user.routes")
+
+
+
+// Connect to MongoDB
+// console.log('PORT:', process.env.PORT);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ecommerce', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error(err));
+
+app.use(express.json());
+app.use(cors());
+// app.use('/api/users', users);
+
+
+app.get('/', (req, res) => {
+  res.send('API WORKING');
+});
+
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+//   });
+  
+
+
